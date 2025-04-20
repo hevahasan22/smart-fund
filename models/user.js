6e09c7d3a47d00817d5a0b0aa02664b5c5f6bd4d
heva22 <hevhasan40@gmail.com>
2025-04-20 17:41:17 +0300

    user

--- models/user.js
+++ models/user.js
@@ -1,116 +1,98 @@
-const mongoose=require('mongoose')
-const joi=require('joi')
-//user schema
-const userSchema=new mongoose.Schema(
-    {
-       userFirstName:
-       {
-         type:String,
-         required:true,
-         trim:true,
-         unique:true,
-         minlength:3,
-         maxlength:20
-       },
-       userLastName:
-       {
-         type:String,
-         required:true,
-         trim:true,
-         unique:true,
-         minlength:3,
-         maxlength:20
-       },
-       email:
-       {
-         type:String,
-         unique:true,
-         required:true,
-         trim:true,
-         minlength:4,
-         maxlength:100
-       },
-       password:
-       {
-         type:String,
-         required:true,
-         minlength:8
-       },
-       phoneNumber:
-       {
-         type:Number,
-         required:true,
-       },
-       DateOfBirth:
-       {
-         type:Date,
-         required:false
-       },
-       address:
-       {
-         type:String,
-         required:false
-       },
-       role: {
-        type: String,
-        enum: ['user', 'admin'],
-        default: 'user'
-      },
-       statues:
-       {
-        type:Boolean,
-        required:false,
-       },
-       accountNumber:
-       {
-        type:Number,
-        required:false
-       },
-       income:
-       {
-        type:String,
-        requires:false
-       },
-       contract:
-       {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'contract'
-       }
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+// User Schema
+const userSchema = new mongoose.Schema(
+  {
+    userFirstName: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 20,
     },
-    {
-      timestamps:true
-    }
-)
+    userLastName: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      minlength: 4,
+      maxlength: 100,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    phoneNumber: {
+      type: Number,
+      required: false,
+    },
+    DateOfBirth: {
+      type: Date,
+      required: false,
+    },
+    address: {
+      type: String,
+      required: false,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+    contract: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'contract',
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Validate Register User
-function validateRegisterUser (obj){
-   const schema=joi.object(
-    {
-      email:joi.string().trim().min(4).max(100).required().email(),
-      userFirstName:joi.string().trim().min(4).max(20).required(),
-      userLastName:joi.string().trim().min(4).max(20).required(),
-      password:joi.string().min(8).required(),
-      phoneNumber:joi.number().required(),
-    }
-   );
-   return schema.validate(obj);
-}
-//validate login user
+function validateRegisterUser(obj) {
+  const schema = Joi.object({
+    email: Joi.string().trim().min(4).max(100).required().email(),
+    userFirstName: Joi.string().trim().min(4).max(20).required(),
+    userLastName: Joi.string().trim().min(4).max(20).required(),
+    password: Joi.string().min(8).required(),
+    phoneNumber: Joi.number(),
+  });
 
-function validateLoginUser(obj){
-  const schema=joi.object(
-    {
-      email:joi.string().trim().min(4).max(100).required().email(),
-      password:joi.string().trim().min(4).max(20).required()
-    }
-  );
-  return schema.validate(obj)
+  return schema.validate(obj);
 }
 
-const userModel=mongoose.model('user',userSchema)
-//export
-module.exports={
-    userModel,
-    validateRegisterUser,
-    validateLoginUser
+// Validate Login User
+function validateLoginUser(obj) {
+  const schema = Joi.object({
+    email: Joi.string().trim().min(4).max(100).required().email(),
+    password: Joi.string().trim().min(4).max(20).required(),
+  });
+
+  return schema.validate(obj);
 }
+
+// Model
+const userModel = mongoose.model('user', userSchema);
+
+// Exports
+module.exports = {
+  userModel,
+  validateRegisterUser,
+  validateLoginUser,
+};
